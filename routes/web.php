0d70557c3b0cf7009f567517c57a37aaf2706efe
Antoine Godbout <1836729@csfoy.ca>
2022-02-22 16:31:23 -0500

    18. Vue Ajax Requests with Axios

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('skills', function (){
+    return ['Laravel', 'Vue', 'PHP', 'JavaScript', 'Tooling'];
+});
